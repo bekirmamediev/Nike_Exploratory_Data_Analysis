93aee79fb8ba9d1f969ddd74886a103fc7ae2b0b
bekirmamediev <bekirmamediev@gmail.com>
2024-07-16 15:48:40 -0400

    add second question

---
+++ rolling_sum_of_order.sql
@@ -0,0 +1,16 @@
+--Create a rolling sum that rolls up the number of order items for each product name 
+--for the Nike Official business ordered by product name.
+--Include the order items where the product name is available.
+
+WITH total AS(
+  SELECT 
+	  pr.product_name 
+    , COUNT(oi.order_item_id) as total_order
+  FROM products pr
+  INNER JOIN order_items oi ON pr.product_id = oi.product_id
+  GROUP BY pr.product_name
+)
+SELECT 
+	product_name 
+  , SUM(total_order)OVER(ORDER BY product_name)
+FROM total; 
