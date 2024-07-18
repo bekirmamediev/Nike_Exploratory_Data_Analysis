44a4b54c47c72d225faea468b7d3ab40ec7f8cee
bekirmamediev <bekirmamediev@gmail.com>
2024-07-18 14:26:25 -0400

    Create q4_Completion_rate_on_two_levels.sql

---
+++ q4_Completion_rate_on_two_levels.sql
@@ -0,0 +1,39 @@
+--Question #4: 
+--Calculate the order item completion rate on two levels of granularity: 
+--(1) The completion rate by age group;
+--(2) The completion rate by age group and product name.
+--Create a table that includes the following columns: age group, order item completion rate by age group, product name, and order item completion rate by age group and product name.
+--Only include customers for who the age group is available.
+
+WITH nike AS (
+	SELECT *
+     FROM order_items
+    UNION ALL 
+    SELECT *
+    FROM order_items_vintage
+), 
+
+completion_per_age as(
+	SELECT 
+	c.age_group
+  , CAST(SUM(CASE WHEN n.delivered_at is NOT NULL and n.returned_at is NULL THEN 1 ELSE 0 END) as decimal)/ CAST(COUNT(*) as decimal) as rate
+    FROM customers c
+    INNER JOIN nike n 
+	ON c.customer_id = n.user_id 
+	GROUP BY c.age_group
+)
+
+SELECT 
+	c.age_group
+  , MAX(rate)
+  , p.product_name
+  , CAST(SUM(CASE WHEN n.delivered_at is NOT NULL and n.returned_at is NULL THEN 1 ELSE 0 END) as decimal)/ CAST(COUNT(*) as decimal) as rate
+FROM customers c
+INNER JOIN nike n 
+ON c.customer_id = n.user_id 
+INNER JOIN products p 
+ON p.product_id = n.product_id
+LEFT JOIN  completion_per_age as cpa
+ON cpa.age_group = c.age_group
+GROUP BY c.age_group, p.product_name
+ORDER BY 1, 3;
